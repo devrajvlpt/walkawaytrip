f35fbc5512ebf61ab48df770a02a6d874ac1d675
Bharathi Raja <11403@ramco.com>
2016-07-24 15:39:51 +0530

    Added the git requirement python

--- walkawaytrip/settings.py
+++ walkawaytrip/settings.py
@@ -124,13 +124,13 @@ USE_TZ = True
 
 PROJECT_ROOT = os.path.normpath(os.path.dirname(__file__))
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR,  '/walkawaytrip/static'),
+    os.path.join(PROJECT_ROOT ,  'static'),
     #'E:/propbuild/static/',
     
 )
