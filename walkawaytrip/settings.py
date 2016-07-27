c59ba83680ed73c63de149359c29baecad65e3de
BHARATHI RAJA <rajavlpt@gmail.com>
2016-07-27 16:02:49 +0530

    Update settings.py

--- walkawaytrip/settings.py
+++ walkawaytrip/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -134,3 +135,16 @@ STATICFILES_DIRS = (
     #'E:/propbuild/static/',
     
 )
+
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+DEBUG = False
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
